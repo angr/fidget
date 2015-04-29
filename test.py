7977c5a4c0040e166f2a99e8f79f244ca296c08d
Andrew Dutcher <andrew@andrewdutcher.com>
2015-04-29 14:57:40 -0700

    Made it use the binaries repository. much cleaner.

--- test.py
+++ test.py
@@ -1,146 +1,34 @@
 #!/usr/bin/env python
-import os, sys, traceback
-dd = os.path.dirname(__file__)
-sd = os.path.realpath(os.path.join(dd, 'tests', 'scripts'))
-sys.path.append(sd)
-os.chdir(dd)
-import tests
 
-def setup_module():
-    os.chdir(dd)
-    os.system('make -C tests')
+import os
+import testinfo
 
-def test_test_arrays_amd64():
-    os.chdir(dd)
-    tests.make_test_function("test_arrays_amd64")()
+testloc = str(os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../binaries/tests')))
 
-def test_test_loops_amd64():
-    os.chdir(dd)
-    tests.make_test_function("test_loops_amd64")()
+arches = ['x86_64', 'i386', 'armel', 'ppc', 'ppc64', 'mips', 'mipsel']
+ctf_binaries = ['x86_64/ctf_shifty', 'i386/ctf_nuclear', 'ppc/ctf_deepblue']
 
-def test_test_division_amd64():
-    os.chdir(dd)
-    tests.make_test_function("test_division_amd64")()
+def test_arrays():
+    for path in arches:
+        binary = os.path.join(testloc, path, 'test_arrays')
+        yield testinfo.test_single_binary, binary
 
-def test_test_arrays_x86():
-    os.chdir(dd)
-    tests.make_test_function("test_arrays_x86")()
+def test_loops():
+    for path in arches:
+        binary = os.path.join(testloc, path, 'test_loops')
+        yield testinfo.test_single_binary, binary
 
-def test_test_loops_x86():
-    os.chdir(dd)
-    tests.make_test_function("test_loops_x86")()
+def test_division():
+    for path in arches:
+        binary = os.path.join(testloc, path, 'test_division')
+        yield testinfo.test_single_binary, binary
 
-def test_test_division_x86():
-    os.chdir(dd)
-    tests.make_test_function("test_division_x86")()
-
-def test_test_arrays_armel():
-    os.chdir(dd)
-    tests.make_test_function("test_arrays_armel")()
-
-def test_test_loops_armel():
-    os.chdir(dd)
-    tests.make_test_function("test_loops_armel")()
-
-def test_test_division_armel():
-    os.chdir(dd)
-    tests.make_test_function("test_division_armel")()
-
-def test_test_arrays_ppc():
-    os.chdir(dd)
-    tests.make_test_function("test_arrays_ppc")()
-
-def test_test_loops_ppc():
-    os.chdir(dd)
-    tests.make_test_function("test_loops_ppc")()
-
-def test_test_division_ppc():
-    os.chdir(dd)
-    tests.make_test_function("test_division_ppc")()
-
-def test_test_arrays_ppc64():
-    os.chdir(dd)
-    tests.make_test_function("test_arrays_ppc64")()
-
-def test_test_loops_ppc64():
-    os.chdir(dd)
-    tests.make_test_function("test_loops_ppc64")()
-
-def test_test_division_ppc64():
-    os.chdir(dd)
-    tests.make_test_function("test_division_ppc64")()
-
-def test_test_arrays_mips():
-    os.chdir(dd)
-    tests.make_test_function("test_arrays_mips")()
-
-def test_test_loops_mips():
-    os.chdir(dd)
-    tests.make_test_function("test_loops_mips")()
-
-def test_test_division_mips():
-    os.chdir(dd)
-    tests.make_test_function("test_division_mips")()
-
-def test_test_arrays_mipsel():
-    os.chdir(dd)
-    tests.make_test_function("test_arrays_mipsel")()
-
-def test_test_loops_mipsel():
-    os.chdir(dd)
-    tests.make_test_function("test_loops_mipsel")()
-
-def test_test_division_mipsel():
-    os.chdir(dd)
-    tests.make_test_function("test_division_mipsel")()
-
-def test_ctf_amd64():
-    os.chdir(dd)
-    tests.make_test_function("ctf_amd64")()
-
-def test_ctf_x86():
-    os.chdir(dd)
-    tests.make_test_function("ctf_x86")()
-
-def test_ctf_ppc():
-    os.chdir(dd)
-    tests.make_test_function("ctf_ppc")()
-
-funcs = [test_test_arrays_amd64, test_test_loops_amd64, test_test_division_amd64, test_test_arrays_x86, test_test_loops_x86, test_test_division_x86, test_test_arrays_armel, test_test_loops_armel, test_test_division_armel, test_test_arrays_ppc, test_test_loops_ppc, test_test_division_ppc, test_test_arrays_ppc64, test_test_loops_ppc64, test_test_division_ppc64, test_test_arrays_mips, test_test_loops_mips, test_test_division_mips, test_test_arrays_mipsel, test_test_loops_mipsel, test_test_division_mipsel, test_ctf_amd64, test_ctf_x86, test_ctf_ppc]
+def test_ctf():
+    for path in ctf_binaries:
+        binary = os.path.join(testloc, path)
+        yield testinfo.test_single_binary, binary
 
 if __name__ == '__main__':
-    setup_module()
-    failures = []
-    errors = []
-    for func in funcs:
-        sys.stdout.write('Running %s... ' % func.func_name)
-        sys.stdout.flush()
-        try:
-            func()
-        except AssertionError:
-            sys.stdout.write('FAIL\n')
-            buf = traceback.format_exc()
-            failures.append((func.func_name, buf))
-        except Exception:
-            sys.stdout.write('ERROR\n')
-            buf = traceback.format_exc()
-            errors.append((func.func_name, buf))
-        except KeyboardInterrupt:
-            sys.stdout.write('INTERRUPTED\n')
-        else:
-            sys.stdout.write('ok\n')
-    for name, buf in failures:
-        print '================================================'
-        print '   FAILED: ' + name
-        print '================================================'
-        print buf
-    print '\n'
-    for name, buf in errors:
-        print '================================================'
-        print '   ERRORED: ' + name
-        print '================================================'
-        print buf
-
-    print '\n'
-    print 'Ran %d tests, %d failures, %d errors' % (len(funcs), len(failures), len(errors))
-
+    for testiter in (test_arrays(), test_loops(), test_division(), test_ctf()):
+        for testfunc in testiter:
+            testfunc[0]()
--- testinfo/__init__.py
+++ testinfo/__init__.py
@@ -9,92 +9,77 @@ LOOPS_OUTPUT = '0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
 DIVISION_OUTPUT = '''3/4 = 0
 9/2 = 4
 4/2 = 2
-10/0 = ''' 
+10/0 = '''
 
-from exploit_amd64   import main as tester_amd64,   always as always_amd64
-from exploit_x86     import main as tester_x86,     always as always_x86
-from exploit_arm     import main as tester_arm,     always as always_arm
-from exploit_ppc     import main as tester_ppc,     always as always_ppc
-from exploit_aarch64 import main as tester_aarch64, always as always_aarch64
+from .exploit_amd64   import main as tester_amd64,   always as always_amd64
+from .exploit_x86     import main as tester_x86,     always as always_x86
+from .exploit_arm     import main as tester_arm,     always as always_arm
+from .exploit_ppc     import main as tester_ppc,     always as always_ppc
+from .exploit_aarch64 import main as tester_aarch64, always as always_aarch64
 
-def make_test_function(binary):
+def test_single_binary(binary):
     ctf = 'ctf_' in binary
     if ctf:
-        if 'ctf_amd64' in binary: tester = tester_amd64; always = always_amd64
-        elif 'ctf_x86' in binary: tester = tester_x86; always = always_x86
-        elif 'ctf_armhf' in binary: tester = tester_arm; always = always_arm
-        elif 'ctf_ppc' in binary: tester = tester_ppc; always = always_ppc
-        elif 'ctf_aarch64' in binary: tester = tester_aarch64; always = always_aarch64
+        if   'x86_64'   in binary: tester = tester_amd64;   always = always_amd64
+        elif 'i386'     in binary: tester = tester_x86;     always = always_x86
+        elif 'armhf'    in binary: tester = tester_arm;     always = always_arm
+        elif 'ppc'      in binary: tester = tester_ppc;     always = always_ppc
+        elif 'aarch64'  in binary: tester = tester_aarch64; always = always_aarch64
         else: raise Exception("What even is this ctf binary?")
-
         winner = CTF_WINNER
     else:
-        if 'test_arrays' in binary: expected = ARRAYS_OUTPUT
-        elif 'test_loops' in binary: expected = LOOPS_OUTPUT
+        if   'test_arrays'   in binary: expected = ARRAYS_OUTPUT
+        elif 'test_loops'    in binary: expected = LOOPS_OUTPUT
         elif 'test_division' in binary: expected = DIVISION_OUTPUT
 
-    def out():
-        if ctf:
-            generic_ctf_test(binary, tester, always, winner)
-        else:
-            generic_test(binary, expected)
-
-    return out
-
+    if ctf:
+        generic_ctf_test(binary, tester, always, winner)
+    else:
+        generic_test(binary, expected)
 
 
 def generic_ctf_test(binary, tester, always, winner):
-    nose.tools.assert_in(binary, os.listdir('tests'))
-
-    #os.chdir('tests')
     #process = boot(binary)
     #try:
-    #    testdata = tester()
+    #    testdata = tester(binary)
     #finally:
     #    process.kill()
-    #    os.chdir('..')
 
     #nose.tools.assert_in(always, testdata)
     #nose.tools.assert_in(winner, testdata)
 
-    fidgetress = Fidget('tests/' + binary)
+    patched_binary = binary + '.out'
+
+    fidgetress = Fidget(binary)
     fidgetress.patch()
     nose.tools.assert_not_equals(len(fidgetress.dump_patches()), 0)
-    fidgetress.apply_patches()
+    fidgetress.apply_patches(patched_binary)
 
-    nose.tools.assert_in(binary + '.out', os.listdir('tests'))
-
-    os.chdir('tests')
-    process = boot(binary + '.out')
+    process = boot(patched_binary)
     try:
-        testdata = tester()
+        testdata = tester(patched_binary)
     finally:
         process.kill()
-        os.chdir('..')
+    os.unlink(patched_binary)
 
     nose.tools.assert_in(always, testdata)
     nose.tools.assert_not_in(winner, testdata)
 
 def generic_test(binary, expected):
-    nose.tools.assert_in(binary, os.listdir('tests'))
+    patched_binary = binary + '.out'
 
-    os.chdir('tests')
     process = boot(binary)
     output = process.output()
-    os.chdir('..')
     nose.tools.assert_in(expected, output)
 
-    fidgetress = Fidget('tests/' + binary)
+    fidgetress = Fidget(binary)
     fidgetress.patch()
     nose.tools.assert_not_equals(len(fidgetress.dump_patches()), 0)
-    fidgetress.apply_patches()
+    fidgetress.apply_patches(patched_binary)
 
-    nose.tools.assert_in(binary + '.out', os.listdir('tests'))
-
-    os.chdir('tests')
-    process = boot(binary + '.out')
+    process = boot(patched_binary)
     output = process.output()
-    os.chdir('..')
+    os.unlink(patched_binary)
     nose.tools.assert_in(expected, output)
 
 
@@ -103,27 +88,27 @@ def boot(binary):
 
     arch = ''
 
-    for myname in arch_bullcrap:
-        if myname in binary and len(myname) > len(arch):
-            arch = myname
+    for archname in arch_bullcrap:
+        if archname in binary and len(archname) > len(arch):
+            arch = archname
     if arch == '':
-        raise Exception('Binary not suffixed properly!')
+        raise Exception('Binary not named properly!')
 
     socketserver = None
-    if binary.startswith('ctf_amd64'):
+    if  'ctf_shifty' in binary:
         socketserver = 1098
-    elif binary.startswith('ctf_ppc'):
+    elif 'ctf_deepblue' in binary:
         socketserver = 8888
-    elif binary.startswith('ctf_armhf'):
+    elif 'ctf_armhf' in binary:
         socketserver = 8000
-    elif binary.startswith('ctf_aarch64'):
+    elif 'ctf_aarch64' in binary:
         socketserver = 4464
 
-    return Process(binary='./' + binary, async=async, arch=arch, socketserver=socketserver)
+    return Process(binary=binary, async=async, arch=arch, socketserver=socketserver)
 
 arch_bullcrap = {
-    'amd64': [],
-    'x86': [],
+    'x86_64': [],
+    'i386': [],
     'ppc': ['qemu-ppc', '-L', '/usr/powerpc-linux-gnu', '-E', 'LD_LIBRARY_PATH=/usr/powerpc-linux-gnu/lib'],
     'ppc64': ['qemu-ppc64', '-L', '/usr/powerpc-linux-gnu', '-E', 'LD_LIBRARY_PATH=/usr/powerpc-linux-gnu/lib64'],
     'armel': ['qemu-arm', '-L', '/usr/arm-linux-gnueabi', '-E', 'LD_LIBRARY_PATH=/usr/arm-linux-gnueabi/lib'],
@@ -131,21 +116,24 @@ arch_bullcrap = {
     'aarch64': ['qemu-aarch64', '-L', '/usr/aarch64-linux-gnu', '-E', 'LD_LIBRARY_PATH=/usr/aarch64-linux-gnu/lib'],
     'mips': ['qemu-mips', '-L', '/usr/mips-linux-gnu', '-E', 'LD_LIBRARY_PATH=/usr/mips-linux-gnu/lib'],
     'mipsel': ['qemu-mipsel', '-L', '/usr/mipsel-linux-gnu', '-E', 'LD_LIBRARY_PATH=/usr/mipsel-linux-gnu/lib'],
-        }
+}
+
+mydir = str(os.path.dirname(os.path.realpath(__file__)))
 
 class Process:
     def __init__(self, binary, async, arch, socketserver):
         command = arch_bullcrap[arch] + [binary]
         if socketserver:
-            command = ['scripts/serve-stdio', ' '.join(command), str(socketserver)]
-        kwargs = {}
+            servestdio = str(os.path.join(mydir, 'serve-stdio'))
+            command = [servestdio, ' '.join(command), str(socketserver)]
+        kwargs = {'cwd': mydir}
         if not async:
             kwargs['stdout'] = subprocess.PIPE
         #print command
         self.process = subprocess.Popen(command, **kwargs)
         if async:
             time.sleep(0.5)
-        
+
     def output(self):
         return self.process.stdout.read()
 
